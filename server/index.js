76e05524e970dc15beac6d4387797b895a7f951e
Moballigh5225 <moballigh5225@gmail.com>
2024-06-15 23:02:15 +0530

    Update .gitignore and server configuration to serve static files from dist

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/dalle", dalleRoutes);
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../client/build")));
+// Serve static files from the client/dist directory
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
